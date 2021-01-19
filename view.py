20eed904c143c6e874a2503861a61cb69ca04392
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-01-19 14:15:22 +0100

    working viewer, color code in function of charging

---
+++ view.py
@@ -0,0 +1,25 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+LOG_FILENAME = 'PyBatteryLog.csv'
+
+data = pd.read_csv(LOG_FILENAME)
+x = data['timestamp']
+x = x - x[0]
+y = data['BatteryLifePercent']
+charging = data['BatteryFlag']
+charging = (charging & 8)
+
+fig, ax = plt.subplots()
+ax.set_xlim(0, x[len(x) - 1] + 10)
+ax.set_ylim(0, 105)
+# ax.plot(x, y)
+ax.scatter(x=x, y=y, c=charging, cmap='bwr_r')
+
+ax.set(xlabel='time (s)', ylabel='BatteryLifePercent (%)')
+ax.grid()
+
+fig.savefig("PyBatteryView.png")
+plt.show()
