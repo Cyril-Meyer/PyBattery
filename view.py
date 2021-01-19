b1fdafcaf98f9e08940411bcf458d1d273014452
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-01-19 18:13:49 +0100

    update view, change some details for better savefig

--- view.py
+++ view.py
@@ -19,10 +19,10 @@ fig, ax = plt.subplots()
 ax.set_xlim(0, x[len(x) - 1] + 10)
 ax.set_ylim(0, 105)
 # ax.plot(x, y)
-ax.scatter(x=x, y=y, c=charging, cmap='bwr_r')
+ax.scatter(x=x, y=y, s=2, c=charging, cmap='bwr_r')
 
 ax.set(xlabel='time (s)', ylabel='BatteryLifePercent (%)')
 ax.grid()
 
-fig.savefig("PyBatteryView.png")
+fig.savefig("PyBatteryView.png", dpi=300)
 plt.show()
